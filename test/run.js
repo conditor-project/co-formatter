97f203bec0857a3a1b9be34ea9320201d8f9feee
Jean-Baptiste Genicot <jean-baptiste.genicot@inist.fr>
2017-10-02 11:18:14 +0200

    correction TU

--- test/run.js
+++ test/run.js
@@ -28,20 +28,27 @@ describe(pkg.name + '/index.js', function () {
 			  process.exit(1);
 		  }
 
-      console.log(docObject);
-      expect(docObject.auteur.value).to.be.equal('Silva Ricardo R. Jourdan Fabien Salvanha Diego M.');
-		  expect(docObject.titre.value).to.be.equal('ProbMetab: an R package for Bayesian probabilistic annotation of LC-MS-based metabolomics');
-		  expect(docObject.issn.value).to.be.equal('1367-4803');
-		  expect(docObject.page.value).to.be.equal('1336 - 1337');
-		  expect(docObject.volume.value).to.be.equal('30');
-      expect(docObject.numero.value).to.be.equal('9');
+      //console.log(docObject);
+      expect(docObject.auteur.value).to.be.equal('Coeurdassier Michaël Berny Philippe Couval Geoffroy');
+		  expect(docObject.titre.value).to.be.equal('Limiting the accidental poisoning of wild and domesticated animals due to the chemical pesticides used to control water vole outbreaks: progress to date');
+      expect(docObject.titrefr.value).to.be.equal('Évolution des effets non intentionnels de la lutte chimique contre le campagnol terrestre sur la faune sauvage et domestique');
+      expect(docObject.issn.value).to.be.equal('0429-2766');
+		  expect(docObject.page.value).to.be.equal('327-335');
+		  expect(docObject.volume.value).to.be.equal('1');
+      expect(docObject.numero.value).to.be.equal('220');
+      expect(docObject.idhal.value).to.be.equal('hal-01103402');
+      expect(docObject.typeDocument.value).to.be.equal('ART');
+      expect(docObject.typeConditor).to.be.equal('Article');
+      expect(docObject.orcid.value).to.be.equal('http://orcid.org/0000-0003-2376-0136');
       expect(docObject.source).to.be.equal('hal');
+      expect(docObject.doi.value).to.be.equal('test-doi');
+      expect(docObject.viaf.value).to.be.equal('test-viaf');
+      expect(docObject.typeDocument.value).to.be.equal('ART');
+      expect(docObject.halauthorid.value).to.be.equal('460031 370098 762273 963831 778917 1116279 963833 828346');
        	 done();
       });
     });
 
-    docObject=testData[1];
-    docObject.path = path.join(__dirname, docObject.path);
     
 
     it('docObject qui renvoie canvasOK @1', function (done) {
@@ -52,7 +59,7 @@ describe(pkg.name + '/index.js', function () {
           process.exit(1);
         }
         
-        console.log(docObject);
+        //console.log(docObject);
         expect(docObject.auteur.value).to.be.not.equal('');
         expect(docObject.auteur.value).to.be.not.undefined;
         expect(docObject.titre.value).to.be.not.equal('');
@@ -69,37 +76,5 @@ describe(pkg.name + '/index.js', function () {
         done();
       });
     });
-
-    docObject=testData[2];
-    docObject.path = path.join(__dirname, docObject.path);
-    
-
-    it('docObject qui renvoie canvasOK @1', function (done) {
-      business.doTheJob( docObject, function (err) {
-        if (err) {
-          console.log(kuler(err.errCode, 'red'));
-          console.log(kuler(err.errMessage, 'red'));
-          process.exit(1);
-        }
-        
-        console.log(docObject);
-       /**
-        expect(docObject.auteur.value).to.be.not.equal('');
-        expect(docObject.auteur.value).to.be.not.undefined;
-        expect(docObject.titre.value).to.be.not.equal('');
-        expect(docObject.titre.value).to.be.not.undefined;
-        expect(docObject.issn.value).to.be.not.equal('');
-        expect(docObject.issn.value).to.be.not.undefined;
-        expect(docObject.page.value).to.be.not.equal('');
-        expect(docObject.page.value).to.be.not.undefined;
-        expect(docObject.volume.value).to.be.not.equal('');
-        expect(docObject.volume.value).to.be.not.undefined;
-        expect(docObject.numero.value).to.be.not.equal('');
-        expect(docObject.numero.value).to.be.not.undefined;
-        */
-        done();
-      });
-    });
-
   });
 });
