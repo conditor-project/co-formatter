782c68f751be6638a8584d7049f0547ef7352854
Jean-Baptiste Genicot <jean-baptiste.genicot@inist.fr>
2018-02-01 10:53:22 +0100

    ajout de traitement rempacant thèse+isbn par ouvrage typologie conditor

--- index.js
+++ index.js
@@ -1,205 +1,237 @@
 'use strict';
 
 const business = {},
-  dom = require('xmldom').DOMParser,
+  Dom = require('xmldom').DOMParser,
   fs = require('fs'),
   xpath = require('xpath'),
   _ = require('lodash'),
   mappingTD = require('co-config/metadata-mappings.json'),
   metadataXpaths = require('co-config/metadata-xpaths.json');
 
-
 business.doTheJob = function (jsonLine, cb) {
-
 
   //console.log(jsonLine.path);
   let error;
   let xml = fs.readFileSync(jsonLine.path, 'utf8');
-  let doc = new dom().parseFromString(xml, 'text/xml',function(err) {
+  let doc = new Dom().parseFromString(xml, 'text/xml', function (err) {
     if (err) {
-        error = {
-            errCode: 1,
-            errMessage: 'erreur de parsing XML : ' + err
-        };
-        jsonLine.errors=[];
-        jsonLine.errors.push(error);
-        return cb(error);
+      error = {
+        errCode: 1,
+        errMessage: 'erreur de parsing XML : ' + err
+      };
+      jsonLine.errors = [];
+      jsonLine
+        .errors
+        .push(error);
+      return cb(error);
     }
   });
   let type_conditor;
-  let doc_bloc,evaluatorOptionsBloc;
-  let namespaces = {'TEI': 'http://www.tei-c.org/ns/1.0', 'xmlns:hal': 'http://hal.archives-ouvertes.fr/'};
-  
-  
+  let doc_bloc,
+    evaluatorOptionsBloc;
+  let namespaces = {
+    'TEI': 'http://www.tei-c.org/ns/1.0',
+    'xmlns:hal': 'http://hal.archives-ouvertes.fr/'
+  };
+
   const evaluatorOptions = {
     node: doc,
     namespaces: namespaces,
     functions: {
       'lower-case': function (context, arg) {
-        return context.contextNode.getAttribute('type').toLowerCase();
+        return context
+          .contextNode
+          .getAttribute('type')
+          .toLowerCase();
       }
     }
   };
   const rawEvaluatorOptions = {
     node: doc,
-    namespaces: namespaces,
+    namespaces: namespaces
   };
   let extractMetadata = {};
-  let select,stringBloc,stringChamps,regexp;
-  let flagSource=false;
-  
-  function matchRegExp(metadata_regexp,value){
-    if (metadata_regexp.regexp && metadata_regexp.regexp!==''){
-      regexp = new RegExp(metadata_regexp.regexp,metadata_regexp.flag);
-      return value.replace(regexp,metadata_regexp.replace);
+  let select,
+    stringBloc,
+    stringChamps,
+    regexp;
+  let flagSource = false;
+
+  function matchRegExp(metadata_regexp, value) {
+    if (metadata_regexp.regexp && metadata_regexp.regexp !== '') {
+      regexp = new RegExp(metadata_regexp.regexp, metadata_regexp.flag);
+      return value.replace(regexp, metadata_regexp.replace);
     }
   }
 
-  function extract(metadata){
+  function extract(metadata) {
 
-    if (metadata.type==='standard'){
-      select = xpath.parse(metadata.path).evaluateString(evaluatorOptions);
-      if (metadata.regexp){
-       select= matchRegExp(metadata,select);
+    if (metadata.type === 'standard') {
+      select = xpath
+        .parse(metadata.path)
+        .evaluateString(evaluatorOptions);
+      if (metadata.regexp) {
+        select = matchRegExp(metadata, select);
       }
       extractMetadata[metadata.name] = select;
-    }
-    else if (metadata.type==='boolean'){
-      select = xpath.parse(metadata.path).evaluateBoolean(evaluatorOptions);
+    } else if (metadata.type === 'boolean') {
+      select = xpath
+        .parse(metadata.path)
+        .evaluateBoolean(evaluatorOptions);
       extractMetadata[metadata.name] = select;
-    }
-    else if (metadata.type==='iteration'){
-      select = xpath.parse(metadata.path).select(evaluatorOptions);
+    } else if (metadata.type === 'iteration') {
+      select = xpath
+        .parse(metadata.path)
+        .select(evaluatorOptions);
       extractMetadata[metadata.name] = [];
-      if (metadata.nb==='all'){
-        _.each(select,(iteSelect)=>{
-          stringChamps = _.get(iteSelect,'firstChild.data','');
-          if (metadata.regexp) stringChamps = matchRegExp(metadata,stringChamps);
-          if (metadata.structured &&  metadata.structured === 'no')
+      if (metadata.nb === 'all') {
+        _.each(select, (iteSelect) => {
+          stringChamps = _.get(iteSelect, 'firstChild.data', '');
+          if (metadata.regexp) {
+            stringChamps = matchRegExp(metadata, stringChamps);
+          }
+          if (metadata.structured && metadata.structured === 'no') {
             extractMetadata[metadata.name].push(stringChamps);
-          else
-            extractMetadata[metadata.name].push({'value':stringChamps});
+          } else {
+            extractMetadata[metadata.name].push({'value': stringChamps});
+          }
         });
-      }
-      else{
-        for (let i=0;i<metadata.nb;i++){
-          if (select[i]){
-            stringChamps = _.get(select[i],'firstChild.data','');
-            if (metadata.regexp){
-              stringChamps = matchRegExp(metadata,stringChamps);
+      } else {
+        for (let i = 0; i < metadata.nb; i++) {
+          if (select[i]) {
+            stringChamps = _.get(select[i], 'firstChild.data', '');
+            if (metadata.regexp) {
+              stringChamps = matchRegExp(metadata, stringChamps);
             }
-            if (metadata.structured &&  metadata.structured === 'no')
+            if (metadata.structured && metadata.structured === 'no') {
               extractMetadata[metadata.name].push(stringChamps);
-            else
-              extractMetadata[metadata.name].push({'value':stringChamps});
-          }
-        }
-      }
-    }
-    else if (metadata.type==='bloc'){
-      select = xpath.parse(metadata.path).select(evaluatorOptions);
-      extractMetadata[metadata.name] = [];
-      stringBloc='';
-      if (metadata.bloc.nb==='all'){
-        _.each(select,(iteSelect)=>{
-          doc_bloc = new dom().parseFromString(iteSelect.toString(), 'text/xml');
-          evaluatorOptionsBloc = {node: doc_bloc, namespaces: namespaces};
-          _.each(metadata.bloc.champs,(metadata_bloc)=>{
-            stringChamps=_.get(xpath.parse(metadata_bloc.path).select(evaluatorOptionsBloc),'[0]firstChild.data','');
-            if (metadata_bloc.regexp){
-              stringChamps = matchRegExp(metadata_bloc,stringChamps);
+            } else {
+              extractMetadata[metadata.name].push({'value': stringChamps});
             }
-            stringBloc+= stringChamps;
-            stringBloc+=metadata.bloc.separateur;
-          });
-          stringBloc+=metadata.separateur;
-        });
-      }
-      else if (typeof metadata.bloc.nb==='number'){
-        for (let j=0;j<metadata.bloc.nb;j++){
-          if (select[j]){
-            doc_bloc = new dom().parseFromString(select[j].toString(), 'text/xml');
-            evaluatorOptionsBloc = {node: doc_bloc, namespaces: namespaces};
-            _.each(metadata.bloc.champs,(metadata_bloc)=>{
-              stringChamps=_.get(xpath.parse(metadata_bloc.path).select(evaluatorOptionsBloc),'[0]firstChild.data','');
-              if (metadata_bloc.regexp){
-                stringChamps = matchRegExp(metadata_bloc,stringChamps);
-              }
-              stringBloc+= stringChamps;
-              stringBloc+=metadata.bloc.separateur;
-            });
-            stringBloc+=metadata.separateur;
           }
         }
       }
-      extractMetadata[metadata.name]=stringBloc;
+    } else if (metadata.type === 'bloc') {
+      select = xpath
+        .parse(metadata.path)
+        .select(evaluatorOptions);
+      extractMetadata[metadata.name] = [];
+      stringBloc = '';
+      if (metadata.bloc.nb === 'all') {
+        _.each(select, (iteSelect) => {
+          doc_bloc = new Dom().parseFromString(iteSelect.toString(), 'text/xml');
+          evaluatorOptionsBloc = {
+            node: doc_bloc,
+            namespaces: namespaces
+          };
+          _.each(metadata.bloc.champs, (metadata_bloc) => {
+            stringChamps = _.get(xpath.parse(metadata_bloc.path).select(evaluatorOptionsBloc), '[0]firstChild.data', '');
+            if (metadata_bloc.regexp) {
+              stringChamps = matchRegExp(metadata_bloc, stringChamps);
+            }
+            stringBloc += stringChamps;
+            stringBloc += metadata.bloc.separateur;
+          });
+          stringBloc += metadata.separateur;
+        });
+      } else if (typeof metadata.bloc.nb === 'number') {
+        for (let j = 0; j < metadata.bloc.nb; j++) {
+          if (select[j]) {
+            doc_bloc = new Dom().parseFromString(select[j].toString(), 'text/xml');
+            evaluatorOptionsBloc = {
+              node: doc_bloc,
+              namespaces: namespaces
+            };
+            _.each(metadata.bloc.champs, (metadata_bloc) => {
+              stringChamps = _.get(xpath.parse(metadata_bloc.path).select(evaluatorOptionsBloc), '[0]firstChild.data', '');
+              if (metadata_bloc.regexp) {
+                stringChamps = matchRegExp(metadata_bloc, stringChamps);
+              }
+              stringBloc += stringChamps;
+              stringBloc += metadata.bloc.separateur;
+            });
+            stringBloc += metadata.separateur;
+          }
+        }
+      }
+      extractMetadata[metadata.name] = stringBloc;
     }
   }
-  
- 
-  try{
-    _.each(metadataXpaths,(metadata)=>{
+
+  try {
+    _.each(metadataXpaths, (metadata) => {
       extract(metadata);
     });
-  }
-  catch (err){
+  } catch (err) {
     error = {
       errCode: 2,
       errMessage: 'erreur d\'extraction de path XML : ' + err
     };
-    jsonLine.errors=[];
-    jsonLine.errors.push(error);
+    jsonLine.errors = [];
+    jsonLine
+      .errors
+      .push(error);
     return cb(error);
   }
-  
-  type_conditor=[];
-  
-  _.each(mappingTD,(mapping)=>{
-    if (mapping.source.trim()===jsonLine.source.toLowerCase().trim()){
+
+  type_conditor = [];
+
+  _.each(mappingTD, (mapping) => {
+    if (mapping.source.trim() === jsonLine.source.toLowerCase().trim()) {
       //constitution du tableau Type Conditor
-      _.each(extractMetadata.typeDocument,(td)=>{
+      _.each(extractMetadata.typeDocument, (td) => {
         type_conditor.push(mapping.mapping[td.value]);
       });
       //flag vérifiant si l'id source est bien présent
-      if (extractMetadata[mapping.nameID].trim()!=='') flagSource=true; 
+      if (extractMetadata[mapping.nameID].trim() !== '') {
+        flagSource = true;
+      }
     }
   });
 
   //Vérification de la présence d'un idSource
-  if (flagSource===false){
+  if (flagSource === false) {
     error = {
       errCode: 3,
       errMessage: 'erreur d\'identification. Pas d\'id source.'
     };
-    jsonLine.errors=[];
-    jsonLine.errors.push(error);
+    jsonLine.errors = [];
+    jsonLine
+      .errors
+      .push(error);
     return cb(error);
   }
 
-  // Si le tableau de type conditor contient Conférence ou Autre 
-  // et qu'un issn ou eissn est présent
-  // alors on ajoute le type conditor Article s il n'est pas déjà présent.
-  if (!_.find(type_conditor,{type:'Article'})
-    && (_.find(type_conditor,{'type':'Conférence'})
-    || _.find(type_conditor,{'type':'Autre'})) 
-    && (extractMetadata.issn.trim()!=='' 
-    || extractMetadata.eissn.trim()!=='')){
-    type_conditor.push({'type':'Article'});
-}
+  // Si le tableau de type conditor contient Conférence ou Autre et qu'un issn ou
+  // eissn est présent alors on ajoute le type conditor Article s il n'est pas
+  // déjà présent.
+  if (!_.find(type_conditor, {type: 'Article'}) && (_.find(type_conditor, {'type': 'Conférence'}) || _.find(type_conditor, {'type': 'Autre'})) && (extractMetadata.issn.trim() !== '' || extractMetadata.eissn.trim() !== '')) {
+    type_conditor.push({'type': 'Article'});
+  }
+
+
+  // Si le tableau de type Conditor contient Thèse et qu'un isbn est présent alors 
+  // On remplace le type conditor Thèse par le type Ouvrage
+
+  if (_.find(type_conditor, {type:'Thèse'} && extractMetadata.isbn.trim()!=='')){
+    _.pull(type_conditor,{type:'Thèse'});
+    type_conditor.push({type:'Ouvrage'});
+  }
 
   //console.log(extractMetadata);
 
-
-  _.each(extractMetadata,(value,key)=>{
-    if (typeof value ==='string'){ 
+  _.each(extractMetadata, (value, key) => {
+    if (typeof value === 'string') {
       value = value.trim();
-      jsonLine[key]={'value':value};
-    }
-    else if (Array.isArray(value) || typeof(value)==='boolean'){
-      jsonLine[key]=value;
-    }
-    else {
-      jsonLine[key]={'value':value};
+      jsonLine[key] = {
+        'value': value
+      };
+    } else if (Array.isArray(value) || typeof(value) === 'boolean') {
+      jsonLine[key] = value;
+    } else {
+      jsonLine[key] = {
+        'value': value
+      };
     }
   });
 
@@ -208,6 +240,5 @@ business.doTheJob = function (jsonLine, cb) {
   return cb();
 
 };
-
 
 module.exports = business;
