a7ae9a5ffbdb43df1aa1240a933ff8977681d58c
Rémy MEJA <remymeja@free.fr>
2018-10-08 14:22:43 +0200

    reformat code for better function export

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 'use strict';
 
-const business = {};
 const Dom = require('xmldom').DOMParser;
 const fs = require('fs');
 const xpath = require('xpath');
 const _ = require('lodash');
 const mappingTD = require('co-config/metadata-mappings.json');
 const metadataXpaths = require('co-config/metadata-xpaths.json');
+
+const namespaces = {
+  'TEI': 'http://www.tei-c.org/ns/1.0',
+  'xmlns:hal': 'http://hal.archives-ouvertes.fr/'
+};
+
+const business = {};
 
 business.doTheJob = function (jsonLine, cb) {
   let error;
@@ -25,10 +31,6 @@ business.doTheJob = function (jsonLine, cb) {
     }
   });
   let typeConditor;
-  let namespaces = {
-    'TEI': 'http://www.tei-c.org/ns/1.0',
-    'xmlns:hal': 'http://hal.archives-ouvertes.fr/'
-  };
 
   const evaluatorOptions = {
     node: doc,
@@ -44,119 +46,11 @@ business.doTheJob = function (jsonLine, cb) {
   };
 
   let extractMetadata = {};
-  let regexp;
   let flagSource = false;
-
-  function matchRegExp (metadataRegexp, value) {
-    if (metadataRegexp.regexp && metadataRegexp.regexp !== '') {
-      regexp = new RegExp(metadataRegexp.regexp, metadataRegexp.flag);
-      return value.replace(regexp, metadataRegexp.replace);
-    }
-  }
-
-  function extract (metadata, contextOptions) {
-    let select;
-
-    if (metadata.type === 'simpleString' && metadata.path) {
-      select = xpath.parse(metadata.path).evaluateString(contextOptions);
-      if (metadata.regexp) {
-        select = matchRegExp(metadata, select);
-      }
-      if (metadata.attributeName && metadata.attributeName.trim() !== '') {
-        let obj = {};
-        obj[metadata.attributeName] = select;
-        select = obj;
-      }
-      return select;
-    } else if (metadata.type === 'boolean' && metadata.path) {
-      select = xpath.parse(metadata.path).evaluateBoolean(contextOptions);
-      if (metadata.attributeName && metadata.attributeName.trim() !== '') {
-        let obj = {};
-        obj[metadata.attributeName] = select;
-        select = obj;
-      }
-      return select;
-    } else if (metadata.type === 'array' && metadata.fields) {
-      let limited = false;
-      let limit = 0;
-
-      if (metadata.limit) {
-        limited = true;
-        limit = metadata.limit;
-      }
-      let result = _.values(_.mapValues(metadata.fields, (field, key) => {
-        if (!limited || limit > 0) {
-          limit--;
-          return extract(field, contextOptions);
-        }
-      }));
-      if (metadata.concat === true && metadata.separator) {
-        let string = '';
-        _.each(result, (field) => {
-          if (string.trim() === '') { string += field; } else { string += metadata.separator + field; }
-        });
-        result = string;
-      }
-      if (metadata.attributeName && metadata.attributeName.trim() !== '') {
-        let obj = {};
-        obj[metadata.attributeName] = result;
-        result = obj;
-      }
-      return result;
-    } else if (metadata.type === 'struct' && metadata.fields) {
-      let obj = {};
-      _.each(metadata.fields, (field) => {
-        obj[field.name] = extract(field, contextOptions);
-      });
-      return obj;
-    } else if (metadata.type === 'bloc' && metadata.path && metadata.fields) {
-      let result = [];
-      let limited = false;
-      let limit = 0;
-
-      if (metadata.limit) {
-        limited = true;
-        limit = metadata.limit;
-      }
-      select = xpath.parse(metadata.path).select(contextOptions);
-      _.each(select, (iteSelect) => {
-        if (!limited || limit > 0) {
-          let docBloc = new Dom().parseFromString(iteSelect.toString(), 'text/xml');
-          let evaluatorOptionsBloc = {
-            node: docBloc,
-            namespaces: namespaces,
-            functions: {
-              'lower-case': function (context, arg) {
-                return context
-                  .contextNode
-                  .getAttribute('type')
-                  .toLowerCase();
-              }
-            }
-          };
-          result.push(extract(metadata.fields, evaluatorOptionsBloc));
-          limit--;
-        }
-      });
-      if (metadata.concat === true && metadata.separator) {
-        let string = '';
-        _.each(result, (field) => {
-          if (string.trim() === '') { string += field; } else { string += metadata.separator + field; }
-        });
-        result = string;
-      }
-      if (metadata.attributeName && metadata.attributeName.trim() !== '') {
-        let obj = {};
-        obj[metadata.attributeName] = result;
-        result = obj;
-      }
-      return result;
-    }
-  }
 
   try {
     _.each(metadataXpaths, (metadata) => {
-      extractMetadata[metadata.name] = extract(metadata, evaluatorOptions);
+      extractMetadata[metadata.name] = this.extract(metadata, evaluatorOptions);
     });
   } catch (err) {
     error = {
@@ -199,22 +93,22 @@ business.doTheJob = function (jsonLine, cb) {
   // Si le tableau de type conditor contient Conférence ou Autre et qu'un issn ou
   // eissn est présent alors on ajoute le type conditor Article s il n'est pas
   // déjà présent.
-  if (!_.find(typeConditor, {type: 'Article'}) && (_.find(typeConditor, {'type': 'Conférence'}) || _.find(typeConditor, {'type': 'Autre'})) && ((extractMetadata.issn && extractMetadata.issn.length > 0) || (extractMetadata.eissn && extractMetadata.eissn.length > 0))) {
-    typeConditor.push({'type': 'Article'});
+  if (!_.find(typeConditor, { type: 'Article' }) && (_.find(typeConditor, { 'type': 'Conférence' }) || _.find(typeConditor, { 'type': 'Autre' })) && ((extractMetadata.issn && extractMetadata.issn.length > 0) || (extractMetadata.eissn && extractMetadata.eissn.length > 0))) {
+    typeConditor.push({ 'type': 'Article' });
   }
 
   // Si le tableau de type Conditor contient Thèse et qu'un isbn est présent alors
   // On remplace le type conditor Thèse par le type Ouvrage
 
-  if (_.find(typeConditor, {type: 'Thèse'}) && extractMetadata.isbn && extractMetadata.isbn.length > 0) {
-    _.pull(typeConditor, {type: 'Thèse'});
-    typeConditor.push({type: 'Ouvrage'});
+  if (_.find(typeConditor, { type: 'Thèse' }) && extractMetadata.isbn && extractMetadata.isbn.length > 0) {
+    _.pull(typeConditor, { type: 'Thèse' });
+    typeConditor.push({ type: 'Ouvrage' });
   }
 
   // Si le tableau de type Conditor contient Conférence et qu'un isbn est présent alors
   // On remplace le type Conditor Conférence par Chapitre
-  if (_.find(typeConditor, {type: 'Conférence'}) && extractMetadata.isbn && extractMetadata.isbn.length > 0) {
-    typeConditor.push({type: 'Chapitre'});
+  if (_.find(typeConditor, { type: 'Conférence' }) && extractMetadata.isbn && extractMetadata.isbn.length > 0) {
+    typeConditor.push({ type: 'Chapitre' });
   }
 
   _.each(extractMetadata, (value, key) => {
@@ -224,6 +118,113 @@ business.doTheJob = function (jsonLine, cb) {
   jsonLine.typeConditor = typeConditor;
 
   return cb();
+};
+
+business.matchRegExp = function (metadataRegexp, value) {
+  if (metadataRegexp.regexp && metadataRegexp.regexp !== '') {
+    const regexp = new RegExp(metadataRegexp.regexp, metadataRegexp.flag);
+    return value.replace(regexp, metadataRegexp.replace);
+  }
+};
+
+business.extract = function (metadata, contextOptions) {
+  let select;
+
+  if (metadata.type === 'simpleString' && metadata.path) {
+    select = xpath.parse(metadata.path).evaluateString(contextOptions);
+    if (metadata.regexp) {
+      select = this.matchRegExp(metadata, select);
+    }
+    if (metadata.attributeName && metadata.attributeName.trim() !== '') {
+      let obj = {};
+      obj[metadata.attributeName] = select;
+      select = obj;
+    }
+    return select;
+  } else if (metadata.type === 'boolean' && metadata.path) {
+    select = xpath.parse(metadata.path).evaluateBoolean(contextOptions);
+    if (metadata.attributeName && metadata.attributeName.trim() !== '') {
+      let obj = {};
+      obj[metadata.attributeName] = select;
+      select = obj;
+    }
+    return select;
+  } else if (metadata.type === 'array' && metadata.fields) {
+    let limited = false;
+    let limit = 0;
+
+    if (metadata.limit) {
+      limited = true;
+      limit = metadata.limit;
+    }
+    let result = _.values(_.mapValues(metadata.fields, (field, key) => {
+      if (!limited || limit > 0) {
+        limit--;
+        return this.extract(field, contextOptions);
+      }
+    }));
+    if (metadata.concat === true && metadata.separator) {
+      let string = '';
+      _.each(result, (field) => {
+        if (string.trim() === '') { string += field; } else { string += metadata.separator + field; }
+      });
+      result = string;
+    }
+    if (metadata.attributeName && metadata.attributeName.trim() !== '') {
+      let obj = {};
+      obj[metadata.attributeName] = result;
+      result = obj;
+    }
+    return result;
+  } else if (metadata.type === 'struct' && metadata.fields) {
+    let obj = {};
+    _.each(metadata.fields, (field) => {
+      obj[field.name] = this.extract(field, contextOptions);
+    });
+    return obj;
+  } else if (metadata.type === 'bloc' && metadata.path && metadata.fields) {
+    let result = [];
+    let limited = false;
+    let limit = 0;
+
+    if (metadata.limit) {
+      limited = true;
+      limit = metadata.limit;
+    }
+    select = xpath.parse(metadata.path).select(contextOptions);
+    _.each(select, (iteSelect) => {
+      if (!limited || limit > 0) {
+        let docBloc = new Dom().parseFromString(iteSelect.toString(), 'text/xml');
+        let evaluatorOptionsBloc = {
+          node: docBloc,
+          namespaces: namespaces,
+          functions: {
+            'lower-case': function (context, arg) {
+              return context
+                .contextNode
+                .getAttribute('type')
+                .toLowerCase();
+            }
+          }
+        };
+        result.push(this.extract(metadata.fields, evaluatorOptionsBloc));
+        limit--;
+      }
+    });
+    if (metadata.concat === true && metadata.separator) {
+      let string = '';
+      _.each(result, (field) => {
+        if (string.trim() === '') { string += field; } else { string += metadata.separator + field; }
+      });
+      result = string;
+    }
+    if (metadata.attributeName && metadata.attributeName.trim() !== '') {
+      let obj = {};
+      obj[metadata.attributeName] = result;
+      result = obj;
+    }
+    return result;
+  }
 };
 
 module.exports = business;
